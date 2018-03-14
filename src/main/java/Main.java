bd8bdec4847e5817696979cd4de6e93c910110c0
Zack Hartmann <zackhartmann@gmail.com>
2018-03-14 12:25:33 -0400

    Implemented List All Exchange Pairs Sought by XPath

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,11 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
 
 public class Main {
 	public static void main(String[] args) {
@@ -14,9 +18,11 @@ public class Main {
 //		driver.get("https://finance.yahoo.com/cryptocurrencies?offset=0&count=150");
 		driver.get("file:///" + absolutePath);
 
+		List<WebElement> elementsList = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[2]/a"));
 
-		System.out.println(driver.getTitle());
-//		List<WebElement> allFormChildElements = driver.findElements(By.xpath("//form[@name='something']/*"));
-
+		System.out.printf("Total Pairs: %s\n\n", elementsList.size());
+		for (WebElement anElement : elementsList) {
+			System.out.println(anElement.getText());
+		}
 	}
 }

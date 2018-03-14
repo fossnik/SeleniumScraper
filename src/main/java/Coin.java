a98dfb3bd31a555bfa5b81ac41455875e2b19ce3
Zack Hartmann <zackhartmann@gmail.com>
2018-03-14 16:11:52 -0400

    Implemented a Coin Class and a Coin List Compiler in the Main Method

---
+++ src/main/java/Coin.java
@@ -0,0 +1,65 @@
+import org.openqa.selenium.WebElement;
+
+import java.security.InvalidParameterException;
+
+public class Coin {
+	private String symbol;
+	private String name;
+	private Double price;
+	private Double change;
+	private Double pChange;
+	private Double marketCap;
+	private Double volume;
+	private Double volume24h;
+	private Double totalVolume24h;
+	private Double circulatingSupply;
+
+	public Coin(WebElement symbol, WebElement name, WebElement price, WebElement change, WebElement pChange, WebElement marketCap, WebElement volume, WebElement volume24h, WebElement totalVolume24h, WebElement circulatingSupply) {
+		this.symbol = symbol.getText();
+		this.name = name.getText();
+		this.price = Double.valueOf(price.getText().replaceAll(",",""));
+		this.change = Double.valueOf(change.getText().replaceAll(",","").replaceAll("%", ""));
+		this.pChange = Double.valueOf(pChange.getText().replaceAll(",","").replaceAll("%", ""));
+		this.marketCap = parseMagnitude(marketCap);
+		this.volume = parseMagnitude(volume);
+		this.volume24h = parseMagnitude(volume24h);
+		this.totalVolume24h = parseMagnitude(totalVolume24h);
+		this.circulatingSupply = parseMagnitude(circulatingSupply);
+	}
+	
+	private Double parseMagnitude(WebElement we) {
+
+		String string = we.getText().replaceAll("[^0-9.MBT]","");
+
+		// M B and T for Millions, Billions, and Trillions. (eg 142.43B	=== 142,000,000,000)
+		switch (string.charAt(string.length() - 1)) {
+			case 'M':
+				return Double.valueOf(string.replaceAll("M", "")) * 1000000D;
+			case 'B':
+				return Double.valueOf(string.replaceAll("B", "")) * 1000000000D;
+			case 'T':
+				return Double.valueOf(string.replaceAll("T", "")) * 1000000000000D;
+			default:
+				if (string.matches("\\D+"))
+					throw new InvalidParameterException("Cannot be converted to valid number");
+				else
+					return Double.valueOf(string);
+		}
+	}
+
+	@Override
+	public String toString() {
+		return "Coin{" +
+				"symbol='" + symbol + '\'' +
+				", name='" + name + '\'' +
+				", price=" + price +
+				", change=" + change +
+				", pChange=" + pChange +
+				", marketCap=" + marketCap +
+				", volume=" + volume +
+				", volume24h=" + volume24h +
+				", totalVolume24h=" + totalVolume24h +
+				", circulatingSupply=" + circulatingSupply +
+				'}';
+	}
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,7 +4,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.File;
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
@@ -18,22 +18,36 @@ public class Main {
 //		driver.get("https://finance.yahoo.com/cryptocurrencies?offset=0&count=150");
 		driver.get("file:///" + absolutePath);
 
-		List<WebElement> columnHeads = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[2]/a"));
-		List<WebElement> rowHeads = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/thead/tr/th[*]/span"));
+		List<WebElement> rows = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[*]/td[2]"));
+		List<WebElement> columns = driver.findElements(By.xpath("//*[@id=\"scr-res-table\"]/table/thead/tr/th[*]/span"));
 
-		// construct matrix of coins and their respective properties/metrics
-		String[][] matrix = new String[columnHeads.size()][rowHeads.size()];
-		for (int column = 0, numPairs = columnHeads.size(); column < numPairs; column++) {
-			WebElement pair = columnHeads.get(column);
+		List<String> symbols = new ArrayList<String>();
+		for (WebElement r : rows)
+			symbols.add(r.getText());
 
-			for (int row = 0, numProperties = rowHeads.size(); row < numProperties; row++) {
-				WebElement property = rowHeads.get(row);
-				matrix[column][row] = property.getText();
-			}
+		List<String> properties = new ArrayList<String>();
+		for (WebElement c : columns)
+			properties.add(c.getText());
 
+		// build a list of coins with their respective properties
+		List<Coin> coins = new ArrayList<Coin>();
+		for (int i = 2; i < symbols.size() + 1; i++) {
+			coins.add(new Coin(
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[2]/a")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[3]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[4]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[5]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[6]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[7]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[8]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[9]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[10]")),
+					driver.findElement(By.xpath("//*[@id=\"scr-res-table\"]/table/tbody/tr[" + i + "]/td[11]"))
+			));
 		}
 
-		// print matrix
-		System.out.println(Arrays.deepToString(matrix));
+		for (Coin coin: coins) {
+			System.out.println(coin.toString());
+		}
 	}
 }

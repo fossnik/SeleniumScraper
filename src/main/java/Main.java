f10cb6fc4b0b7d0f4b2fab144094b16beed3a330
Zack Hartmann <zackhartmann@gmail.com>
2018-05-08 01:25:48 -0400

    Implemented Relational Database

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,15 +6,16 @@ public class Main {
 		Scraper scraper = new Scraper("https://finance.yahoo.com/cryptocurrencies?offset=0&count=150");
 
 		try {
+			System.out.println("\nScraping...");
 			List<Coin> coins = scraper.compileSnapshot();
-
 			for (Coin coin: coins)
 				System.out.println(coin.toString());
 
+			System.out.println("\nScraping Completed - Persisting to DB...");
 			if (Snapshot.commitSnapshot(coins))
-				System.out.println("\n Snapshot Successfully Committed to DB");
+				System.out.println("\n\tSnapshot Successfully Committed to DB");
 			else
-				System.out.println("\n Failed to Push to database");
+				System.out.println("\n\tFailed to Push to database");
 
 			scraper.chromeDriver.quit();
 			System.exit(0);
--- src/main/java/Snapshot.java
+++ src/main/java/Snapshot.java
@@ -19,66 +19,62 @@ class Snapshot {
 	private static final String COLUMN_TOTALVOLUME24H = "totalVolume24h";
 	private static final String COLUMN_CIRCULATINGSUPPLY = "circulatingSupply";
 
+	private static final String COIN_NAMES_AND_SYMBOLS_TABLE = "all_your_coin";
+
 	static boolean commitSnapshot(List<Coin> coins) {
-		// create connection
 		if (!createConnection()) return false;
+
+		if (!createTableOfAllCoinsNamesAndSymbols()) return false;
 
 		// attempt to create tables for all coins
 		for (Coin coin: coins) {
-
-			// create coin tables (if not exist)
-			if (!createTables(coin)) return false;
-
-			// insert record
+			if (!createIndividualCoinTable(coin)) return false;
 			if (!insertRecord(coin)) return false;
 		}
 
-		// close connection
 		return closeConnection();
 	}
 
-	private static boolean insertRecord(Coin c) {
-		// conform to table title strictures
-		String TABLE_TITLE = c.getSymbol().toLowerCase().replaceAll("[^a-z]", "");
-		String INSERT_SNAPSHOT =
-				"INSERT INTO " + TABLE_TITLE + " values(NULL,CURRENT_TIMESTAMP,?,?,?,?,?,?,?,?,?,?);";
+	private static boolean createTableOfAllCoinsNamesAndSymbols() {
+		String CREATE_COIN_NAMES_AND_SYMBOLS_TABLE =
+				"CREATE TABLE IF NOT EXISTS " + COIN_NAMES_AND_SYMBOLS_TABLE +
+						"(" +
+						COLUMN_SYMBOL + " TEXT PRIMARY KEY, " +
+						COLUMN_NAME + " TEXT" +
+						");"
+				;
+
 		try {
-			PreparedStatement insertCoin = conn.prepareStatement(INSERT_SNAPSHOT);
-			insertCoin.setString(1, c.getSymbol());
-			insertCoin.setString(2, c.getName());
-			insertCoin.setDouble(3, c.getPrice());
-			insertCoin.setDouble(4, c.getChange());
-			insertCoin.setDouble(5, c.getpChange());
-			insertCoin.setDouble(6, c.getMarketCap());
-			insertCoin.setDouble(7, c.getVolume());
-			insertCoin.setDouble(8, c.getVolume24h());
-			insertCoin.setDouble(9, c.getTotalVolume24h());
-			insertCoin.setDouble(10, c.getCirculatingSupply());
-			insertCoin.executeUpdate();
-			insertCoin.close();
+			PreparedStatement createTable = conn.prepareStatement(CREATE_COIN_NAMES_AND_SYMBOLS_TABLE);
+			createTable.execute();
+			createTable.close();
 		} catch (SQLException e) {
-			System.out.println("Couldn't insert data: " + e.getMessage());
+			System.out.println("Failed constructing table of all coin names/symbols\n" +
+					"QUERY:\t" + CREATE_COIN_NAMES_AND_SYMBOLS_TABLE + '\n' + e.getMessage());
 			return false;
 		}
 		return true;
 	}
 
-	private static boolean createTable(String TABLE_TITLE) {
-		String CREATE_COIN_TABLE =
+	private static boolean createIndividualCoinTable(Coin coin) {
+		String TABLE_TITLE = getTable_title(coin);
+
+		String CREATE_SNAPSHOT_RECORDS_TABLE =
 				"CREATE TABLE IF NOT EXISTS " + TABLE_TITLE +
 						"(" +
 						"ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
 						COLUMN_DATETIME + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-						COLUMN_SYMBOL + " String, " +
-						COLUMN_NAME + " String, " +
-						COLUMN_PRICE + " Double," +
+						COLUMN_SYMBOL + " TEXT, " +
+						COLUMN_PRICE + " Double, " +
 						COLUMN_CHANGE + " Double, " +
 						COLUMN_PCHANGE + " Double, " +
 						COLUMN_MARKETCAP + " Double, " +
 						COLUMN_VOLUME + " Double, " +
 						COLUMN_VOLUME24H + " Double, " +
 						COLUMN_TOTALVOLUME24H + " Double, " +
-						COLUMN_CIRCULATINGSUPPLY + " Double" +
+						COLUMN_CIRCULATINGSUPPLY + " Double, " +
+						"FOREIGN KEY(" + COLUMN_SYMBOL + ") REFERENCES " +
+						COIN_NAMES_AND_SYMBOLS_TABLE + "(" + COLUMN_SYMBOL + ")" +
 						");"
 				;
 
@@ -87,20 +83,64 @@ class Snapshot {
 			createTable.execute();
 			createTable.close();
 		} catch (SQLException e) {
-			System.out.printf("Couldn't create table for coin {%s}\n%s\n", TABLE_TITLE, e.getMessage());
+			System.out.println("Failed creating of individual coin table\n" +
+					"QUERY:\t" + CREATE_SNAPSHOT_RECORDS_TABLE + '\n' + e.getMessage());
 			return false;
 		}
 		return true;
 	}
 
+	private static boolean insertRecord(Coin c) {
+		// add this coin to the table of all coin names and symbols (if not exist)
+		String INSERT_COINNAME = String.format("INSERT OR IGNORE INTO %s values(?,?);", COIN_NAMES_AND_SYMBOLS_TABLE);
+		try {
+			PreparedStatement insertCoin = conn.prepareStatement(INSERT_COINNAME);
+			insertCoin.setString(1, c.getSymbol());
+			insertCoin.setString(2, c.getName());
+			insertCoin.executeUpdate();
+			insertCoin.close();
+		} catch (SQLException e) {
+			System.out.println("Failed appending coin to Table of all coins\n" +
+					"QUERY:\t" + INSERT_COINNAME + '\n' + e.getMessage());
+			return false;
+		}
+
+		String TABLE_TITLE = getTable_title(c);
+
+		String INSERT_SNAPSHOT =
+				"INSERT INTO " + TABLE_TITLE + " values(NULL,CURRENT_TIMESTAMP,?,?,?,?,?,?,?,?,?);";
+		try {
+			PreparedStatement insertSnap = conn.prepareStatement(INSERT_SNAPSHOT);
+			insertSnap.setString(1, c.getSymbol());
+			insertSnap.setDouble(2, c.getPrice());
+			insertSnap.setDouble(3, c.getChange());
+			insertSnap.setDouble(4, c.getpChange());
+			insertSnap.setDouble(5, c.getMarketCap());
+			insertSnap.setDouble(6, c.getVolume());
+			insertSnap.setDouble(7, c.getVolume24h());
+			insertSnap.setDouble(8, c.getTotalVolume24h());
+			insertSnap.setDouble(9, c.getCirculatingSupply());
+			insertSnap.executeUpdate();
+			insertSnap.close();
+		} catch (SQLException e) {
+			System.out.println("Failed inserting snapshot record\n" +
+					"QUERY:\t" + INSERT_SNAPSHOT + '\n' + e.getMessage());
+			return false;
+		}
+		return true;
+	}
+
+	private static String getTable_title(Coin c) {
+		// normalize input and conform to SQL strictures for table titles
+		return c.getSymbol().toLowerCase().replaceAll("[^a-z]", "");
+	}
+
 	private static boolean createConnection() {
-		if(conn == null) {
-			try {
-				conn = DriverManager.getConnection(CONNECTION_STRING);
-			} catch (SQLException e) {
-				System.out.println("Couldn't connect to database: " + e.getMessage());
-				return false;
-			}
+		try {
+			conn = DriverManager.getConnection(CONNECTION_STRING);
+		} catch (SQLException e) {
+			System.out.println("Couldn't connect to database: " + e.getMessage());
+			return false;
 		}
 		return true;
 	}

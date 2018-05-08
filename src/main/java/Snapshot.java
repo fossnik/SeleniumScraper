1b59425461e711dc4ea7f95ba6e0443d19928fbf
Zack Hartmann <zackhartmann@gmail.com>
2018-05-08 11:49:19 -0400

    Implemented Safe Symbol Names (for SQL and REST query Safety)

--- src/main/java/Snapshot.java
+++ src/main/java/Snapshot.java
@@ -6,9 +6,8 @@ class Snapshot {
 	private static final String CONNECTION_STRING = "jdbc:sqlite:src/main/resources/RelationalSnapshot.db";
 	private static Connection conn;
 
-	// SQL vocabulary
+	// SQL Vocabulary - Individual Coin Tables
 	private static final String COLUMN_DATETIME = "dateCreated";
-	private static final String COLUMN_SYMBOL = "symbol";
 	private static final String COLUMN_NAME = "name";
 	private static final String COLUMN_PRICE = "price";
 	private static final String COLUMN_CHANGE = "change";
@@ -19,7 +18,10 @@ class Snapshot {
 	private static final String COLUMN_TOTALVOLUME24H = "totalVolume24h";
 	private static final String COLUMN_CIRCULATINGSUPPLY = "circulatingSupply";
 
-	private static final String COIN_NAMES_AND_SYMBOLS_TABLE = "all_your_coin";
+	// SQL Vocabulary - Table of all Names & Symbols
+	private static final String COIN_NAMES_AND_SYMBOLS_TABLE = "_all_your_coin";
+	private static final String COLUMN_SYMBOL_SAFE = "symbol_safe";
+	private static final String COLUMN_SYMBOL_FULL = "symbol_full";
 
 	static boolean commitSnapshot(List<Coin> coins) {
 		if (!createConnection()) return false;
@@ -39,7 +41,8 @@ class Snapshot {
 		String CREATE_COIN_NAMES_AND_SYMBOLS_TABLE =
 				"CREATE TABLE IF NOT EXISTS " + COIN_NAMES_AND_SYMBOLS_TABLE +
 						"(" +
-						COLUMN_SYMBOL + " TEXT PRIMARY KEY, " +
+						COLUMN_SYMBOL_SAFE + " TEXT PRIMARY KEY, " +
+						COLUMN_SYMBOL_FULL + " TEXT, " +
 						COLUMN_NAME + " TEXT" +
 						");"
 				;
@@ -49,7 +52,7 @@ class Snapshot {
 			createTable.execute();
 			createTable.close();
 		} catch (SQLException e) {
-			System.out.println("Failed constructing table of all coin names/symbols\n" +
+			System.out.println("-FAIL db- initial creation of names & symbols table\n" +
 					"QUERY:\t" + CREATE_COIN_NAMES_AND_SYMBOLS_TABLE + '\n' + e.getMessage());
 			return false;
 		}
@@ -57,14 +60,29 @@ class Snapshot {
 	}
 
 	private static boolean createIndividualCoinTable(Coin coin) {
-		String TABLE_TITLE = getTable_title(coin);
+		// add this coin to the table of all coin names and symbols (if not exist)
+		String INSERT_COINNAME = "INSERT OR IGNORE INTO " + COIN_NAMES_AND_SYMBOLS_TABLE + " values(?,?,?);";
 
+		try {
+			PreparedStatement insertCoin = conn.prepareStatement(INSERT_COINNAME);
+			insertCoin.setString(1, getSafeSymbol(coin));
+			insertCoin.setString(2, coin.getSymbol());
+			insertCoin.setString(3, coin.getName());
+			insertCoin.executeUpdate();
+			insertCoin.close();
+		} catch (SQLException e) {
+			System.out.println("-FAIL db- add new coin to names & symbols table\n" +
+					"QUERY:\t" + INSERT_COINNAME + '\n' + e.getMessage());
+			return false;
+		}
+
+		String TABLE_TITLE = getSafeSymbol(coin);
 		String CREATE_SNAPSHOT_RECORDS_TABLE =
 				"CREATE TABLE IF NOT EXISTS " + TABLE_TITLE +
 						"(" +
 						"ID INTEGER PRIMARY KEY AUTOINCREMENT, " +
 						COLUMN_DATETIME + " TIMESTAMP DEFAULT CURRENT_TIMESTAMP, " +
-						COLUMN_SYMBOL + " TEXT, " +
+						COLUMN_SYMBOL_SAFE + " TEXT, " +
 						COLUMN_PRICE + " Double, " +
 						COLUMN_CHANGE + " Double, " +
 						COLUMN_PCHANGE + " Double, " +
@@ -73,8 +91,8 @@ class Snapshot {
 						COLUMN_VOLUME24H + " Double, " +
 						COLUMN_TOTALVOLUME24H + " Double, " +
 						COLUMN_CIRCULATINGSUPPLY + " Double, " +
-						"FOREIGN KEY(" + COLUMN_SYMBOL + ") REFERENCES " +
-						COIN_NAMES_AND_SYMBOLS_TABLE + "(" + COLUMN_SYMBOL + ")" +
+						"FOREIGN KEY(" + COLUMN_SYMBOL_SAFE + ") REFERENCES " +
+						COIN_NAMES_AND_SYMBOLS_TABLE + "(" + COLUMN_SYMBOL_SAFE + ")" +
 						");"
 				;
 
@@ -83,7 +101,7 @@ class Snapshot {
 			createTable.execute();
 			createTable.close();
 		} catch (SQLException e) {
-			System.out.println("Failed creating of individual coin table\n" +
+			System.out.println("-FAIL db- create individual coin table\n" +
 					"QUERY:\t" + CREATE_SNAPSHOT_RECORDS_TABLE + '\n' + e.getMessage());
 			return false;
 		}
@@ -91,27 +109,13 @@ class Snapshot {
 	}
 
 	private static boolean insertRecord(Coin c) {
-		// add this coin to the table of all coin names and symbols (if not exist)
-		String INSERT_COINNAME = String.format("INSERT OR IGNORE INTO %s values(?,?);", COIN_NAMES_AND_SYMBOLS_TABLE);
-		try {
-			PreparedStatement insertCoin = conn.prepareStatement(INSERT_COINNAME);
-			insertCoin.setString(1, c.getSymbol());
-			insertCoin.setString(2, c.getName());
-			insertCoin.executeUpdate();
-			insertCoin.close();
-		} catch (SQLException e) {
-			System.out.println("Failed appending coin to Table of all coins\n" +
-					"QUERY:\t" + INSERT_COINNAME + '\n' + e.getMessage());
-			return false;
-		}
-
-		String TABLE_TITLE = getTable_title(c);
-
+		String TABLE_TITLE = getSafeSymbol(c);
 		String INSERT_SNAPSHOT =
 				"INSERT INTO " + TABLE_TITLE + " values(NULL,CURRENT_TIMESTAMP,?,?,?,?,?,?,?,?,?);";
+
 		try {
 			PreparedStatement insertSnap = conn.prepareStatement(INSERT_SNAPSHOT);
-			insertSnap.setString(1, c.getSymbol());
+			insertSnap.setString(1, getSafeSymbol(c));
 			insertSnap.setDouble(2, c.getPrice());
 			insertSnap.setDouble(3, c.getChange());
 			insertSnap.setDouble(4, c.getpChange());
@@ -123,36 +127,33 @@ class Snapshot {
 			insertSnap.executeUpdate();
 			insertSnap.close();
 		} catch (SQLException e) {
-			System.out.println("Failed inserting snapshot record\n" +
+			System.out.println("-FAIL db- insert snapshot record\n" +
 					"QUERY:\t" + INSERT_SNAPSHOT + '\n' + e.getMessage());
 			return false;
 		}
 		return true;
 	}
 
-	private static String getTable_title(Coin c) {
+	private static String getSafeSymbol(Coin c) {
 		// normalize input and conform to SQL strictures for table titles
 		return c.getSymbol().toLowerCase().replaceAll("[^a-z]", "");
 	}
 
 	private static boolean createConnection() {
-		try {
-			conn = DriverManager.getConnection(CONNECTION_STRING);
-		} catch (SQLException e) {
-			System.out.println("Couldn't connect to database: " + e.getMessage());
+		try { conn = DriverManager.getConnection(CONNECTION_STRING); }
+		catch (SQLException e) {
+			System.out.println("-FAIL db- getConnection: " + e.getMessage());
 			return false;
 		}
 		return true;
 	}
 
 	private static boolean closeConnection() {
-		try {
-			conn.close();
-		} catch(SQLException e) {
-			System.out.println("Couldn't close connection: " + e.getMessage());
+		try { conn.close(); }
+		catch(SQLException e) {
+			System.out.println("-FAIL db- close: " + e.getMessage());
 			return false;
 		}
 		return true;
 	}
-
 }
